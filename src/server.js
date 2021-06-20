5eaf4a81ed29418e6e3dd44ed61898623e0884d7
jeichlcof <janual1102@gmail.com>
2021-06-20 18:31:57 +0900

    .gitignore

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("i stillefsefsfs");
+};
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}✅`);
+
+app.listen(PORT, handleListening);
